28145468b18ed32fd9760e06d4a5832397c6b10c
GK <gisela0108@github.com>
2018-01-28 23:53:16 +0700

    2801_UpdateUIStringDimes
    Update UI income, outcome, pemasukan, dan pengeluaran. Update dimens dan string

---
+++ app/src/main/java/com/gisela/praktikum01/pengeluaran.java
@@ -0,0 +1,13 @@
+package com.gisela.praktikum01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class pengeluaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengeluaran);
+    }
+}
